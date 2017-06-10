26236051e1ce55ec7a48aa8f0f6fc7f52c9f03d4
Maíra <ink.dheart@gmail.com>
2017-06-10 14:06:53 -0300

    Metroform adicionado ao formulário de seleção de autores do livro

--- ProjetoMVC_Livraria/Livraria/View/Livros/FormSelecionarAutores.Designer.cs
+++ ProjetoMVC_Livraria/Livraria/View/Livros/FormSelecionarAutores.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvAutoresLivro = new System.Windows.Forms.DataGridView();
             this.idAutorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nomeAutorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -38,14 +38,15 @@
             this.autorDataSet = new Livraria.AutorDataSet();
             this.autorTableAdapter = new Livraria.AutorDataSetTableAdapters.AutorTableAdapter();
             this.autorDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnFinalizar = new System.Windows.Forms.Button();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnDesmarcarTodos = new System.Windows.Forms.Button();
+            this.picLogoInicial = new System.Windows.Forms.PictureBox();
+            this.btnFinalizar = new MetroFramework.Controls.MetroButton();
+            this.btnCancelar = new MetroFramework.Controls.MetroButton();
+            this.btnDesmarcarTodos = new MetroFramework.Controls.MetroButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAutoresLivro)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogoInicial)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvAutoresLivro
@@ -54,21 +55,21 @@
             this.dgvAutoresLivro.AllowUserToDeleteRows = false;
             this.dgvAutoresLivro.AutoGenerateColumns = false;
             this.dgvAutoresLivro.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvAutoresLivro.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAutoresLivro.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvAutoresLivro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvAutoresLivro.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.idAutorDataGridViewTextBoxColumn,
             this.nomeAutorDataGridViewTextBoxColumn,
             this.SelecionarAutor});
             this.dgvAutoresLivro.DataSource = this.autorBindingSource;
-            this.dgvAutoresLivro.Location = new System.Drawing.Point(67, 62);
+            this.dgvAutoresLivro.Location = new System.Drawing.Point(23, 62);
             this.dgvAutoresLivro.Name = "dgvAutoresLivro";
             this.dgvAutoresLivro.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvAutoresLivro.Size = new System.Drawing.Size(543, 265);
@@ -114,73 +115,69 @@
             this.autorDataSetBindingSource.DataSource = this.autorDataSet;
             this.autorDataSetBindingSource.Position = 0;
             // 
-            // label1
+            // picLogoInicial
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(150, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(376, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Selecione os autores para o livro que está sendo cadastrado!";
+            this.picLogoInicial.BackColor = System.Drawing.Color.Transparent;
+            this.picLogoInicial.Image = global::Livraria.Properties.Resources.logo;
+            this.picLogoInicial.Location = new System.Drawing.Point(23, 21);
+            this.picLogoInicial.Name = "picLogoInicial";
+            this.picLogoInicial.Size = new System.Drawing.Size(38, 33);
+            this.picLogoInicial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogoInicial.TabIndex = 40;
+            this.picLogoInicial.TabStop = false;
             // 
             // btnFinalizar
             // 
-            this.btnFinalizar.FlatAppearance.BorderSize = 20;
-            this.btnFinalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFinalizar.Location = new System.Drawing.Point(512, 337);
+            this.btnFinalizar.Location = new System.Drawing.Point(491, 333);
             this.btnFinalizar.Name = "btnFinalizar";
-            this.btnFinalizar.Size = new System.Drawing.Size(98, 32);
-            this.btnFinalizar.TabIndex = 2;
+            this.btnFinalizar.Size = new System.Drawing.Size(75, 35);
+            this.btnFinalizar.TabIndex = 41;
             this.btnFinalizar.Text = "Finalizar";
-            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.UseSelectable = true;
             this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
             // 
             // btnCancelar
             // 
-            this.btnCancelar.FlatAppearance.BorderSize = 20;
-            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelar.Location = new System.Drawing.Point(391, 337);
+            this.btnCancelar.Location = new System.Drawing.Point(410, 333);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(98, 32);
-            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Size = new System.Drawing.Size(75, 35);
+            this.btnCancelar.TabIndex = 42;
             this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.UseSelectable = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnDesmarcarTodos
             // 
-            this.btnDesmarcarTodos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDesmarcarTodos.Location = new System.Drawing.Point(67, 337);
+            this.btnDesmarcarTodos.Location = new System.Drawing.Point(23, 333);
             this.btnDesmarcarTodos.Name = "btnDesmarcarTodos";
-            this.btnDesmarcarTodos.Size = new System.Drawing.Size(122, 32);
-            this.btnDesmarcarTodos.TabIndex = 4;
+            this.btnDesmarcarTodos.Size = new System.Drawing.Size(128, 35);
+            this.btnDesmarcarTodos.TabIndex = 43;
             this.btnDesmarcarTodos.Text = "Desmarcar Todos";
-            this.btnDesmarcarTodos.UseVisualStyleBackColor = true;
+            this.btnDesmarcarTodos.UseSelectable = true;
             this.btnDesmarcarTodos.Click += new System.EventHandler(this.btnDesmarcarTodos_Click);
             // 
             // FormSelecionarAutores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(677, 389);
+            this.ClientSize = new System.Drawing.Size(586, 380);
             this.Controls.Add(this.btnDesmarcarTodos);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnFinalizar);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picLogoInicial);
             this.Controls.Add(this.dgvAutoresLivro);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FormSelecionarAutores";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormSelecionarAutores";
+            this.Resizable = false;
+            this.Text = "        Selecionar autores para o livro cadastrado";
             this.Load += new System.EventHandler(this.FormSelecionarAutores_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvAutoresLivro)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogoInicial)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -191,12 +188,12 @@
         private System.Windows.Forms.BindingSource autorBindingSource;
         private AutorDataSetTableAdapters.AutorTableAdapter autorTableAdapter;
         private System.Windows.Forms.BindingSource autorDataSetBindingSource;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnFinalizar;
-        private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.DataGridViewTextBoxColumn idAutorDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn nomeAutorDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewCheckBoxColumn SelecionarAutor;
-        private System.Windows.Forms.Button btnDesmarcarTodos;
+        private System.Windows.Forms.PictureBox picLogoInicial;
+        private MetroFramework.Controls.MetroButton btnFinalizar;
+        private MetroFramework.Controls.MetroButton btnCancelar;
+        private MetroFramework.Controls.MetroButton btnDesmarcarTodos;
     }
 }
