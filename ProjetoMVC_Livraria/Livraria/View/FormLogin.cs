50030e04f61deabaeb41b05e6c06ee5774789c5f
Maíra <ink.dheart@gmail.com>
2017-06-10 18:57:38 -0300

    Ajustes gerais na tela de login: input usuário com foco ao carregar e diminuição da altura da tela

--- ProjetoMVC_Livraria/Livraria/View/FormLogin.cs
+++ ProjetoMVC_Livraria/Livraria/View/FormLogin.cs
@@ -114,5 +114,10 @@ namespace Livraria
             new FormSobre().ShowDialog(this);
         }
 
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtUsuario;
+        }
+
     }
 }
